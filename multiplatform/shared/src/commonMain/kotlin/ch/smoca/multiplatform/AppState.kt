5da06d52ae4787da63d6f1bf4e26a315b8176bdf
4nd2in <andrin.weiler@smoca.ch>
2024-06-28 17:30:49 +0200

    Use top level store setup function

--- multiplatform/shared/src/commonMain/kotlin/ch/smoca/multiplatform/AppState.kt
+++ multiplatform/shared/src/commonMain/kotlin/ch/smoca/multiplatform/AppState.kt
@@ -1,6 +1,10 @@
 package ch.smoca.multiplatform
 
+import ch.smoca.multiplatform.reducers.CatFactReducer
+import ch.smoca.multiplatform.reducers.CountReducer
+import ch.smoca.multiplatform.sagas.CatFactNetworkSaga
 import ch.smoca.redux.State
+import ch.smoca.redux.Store
 import kotlinx.serialization.Serializable
 
 // the state describes the data of the whole system
@@ -12,3 +16,14 @@ data class AppState(
     val count: Int = 0,
     val catFact: String = ""
 ): State
+
+// top level function to setup the store with AppState
+fun setupStore(initialState: AppState = AppState()): Store<AppState> =
+    Store(
+        initialState = initialState,
+        sagas = listOf(CatFactNetworkSaga()),
+        reducers = listOf(
+            CatFactReducer(),
+            CountReducer()
+        )
+    )
